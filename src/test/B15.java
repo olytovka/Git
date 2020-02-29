0f12fac3eac532fd4f74babd3aeca2de5c41f58d
Olha Lytovka <olha@Olhas-MacBook-Air.local>
2020-02-29 12:34:33 -0500

    update from intelliJ

--- src/test/B15.java
+++ src/test/B15.java
@@ -3,5 +3,13 @@ package test;
 public class B15 {
     public static void main(String[] args) {
         System.out.println("My file from GitHib");
+        //difference between ++x and x++
+        int x = 2;
+        if(++x < 3){
+            System.out.println(true);
+            System.out.println(x);
+        }else{
+            System.out.println(false);
+        }
     }
 }
--- src/test/ReserveRoom.java
+++ src/test/ReserveRoom.java
@@ -2,4 +2,5 @@ package test;
 
 public class ReserveRoom {
 
+    //only team lead can reserve a room
 }
