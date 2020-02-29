2d83156ce0440d66c8442d5cd0568a685ecc3c43
Olha Lytovka <olha@Olhas-MacBook-Air.local>
2020-02-29 11:57:08 -0500

    ReserveRoom.java

--- src/test/B15.java
+++ src/test/B15.java
@@ -1,5 +1,7 @@
+package test;
 // this is file frim GitHub. Hi B15!
-
-public static void main(String [] args){
-System.out.println("My file from GitHib");
+public class B15 {
+    public static void main(String[] args) {
+        System.out.println("My file from GitHib");
+    }
 }
